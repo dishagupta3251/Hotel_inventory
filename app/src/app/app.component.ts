a8b11c0506e55fc82a13d527ceed73c9023731dc
Disha <dishaguptalko@gmail.com>
2024-05-29 22:36:55 +0530

    commit

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component,  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RoomsComponent } from './rooms/rooms.component';
 import { ContainerComponent } from './container/container.component';
+import { EmployeeComponent } from "./employee/employee.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, RoomsComponent, ContainerComponent]
+    imports: [RouterOutlet, RoomsComponent, ContainerComponent, EmployeeComponent]
 })
 export class AppComponent {
   title = 'app';
